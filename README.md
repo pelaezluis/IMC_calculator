0e7ee1ffad20cf55c89d285b645b31a5a416c386
pelaezluis <luispelaezbedoya@gmail.com>
2020-08-10 00:02:11 -0500

    agregadas funciones para el calculo de imc

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # IMC Calculator
 
-Este programa ha sido creado con el fin de calcular de manera rápida tu indice de masa corporal.
+Este es programa sencillo creado con el fin de calcular de manera rápida tu índice de masa corporal.
--- imc.py
+++ imc.py
@@ -1,40 +1,65 @@
-#This program calculates the IMC (Indice de Masa Corporal)
+"""
+imc_calculator, es un programa que calcula tu Indice de Masa Corporal (IMC)
+y te indica en que rango te encuentras
+"""
 from os import system, name
 
-# First we clear the console depending what OS we have
 def limpiarPantalla():
+    """
+    Detecta que OS se está usando para así limpiar la
+    pantalla correctamente.
+    """
     if name == "posix":
         system ("clear")
     elif name == "ce" or name == "nt" or name == "dos":
         system ("cls")
 
-limpiarPantalla()
 
-print ("\n***** Bienvenido a IMC calculator! *****\n")
-peso = float(input("Ingresa tu peso en kilogramos: "))
-estatura = float(input("Ingresa tu estatura en metros: "))
+def entrada_datos():
+    """
+    Recibe el peso y la estatura de la persona
+    """
+    print ("\n***** Bienvenido a IMC calculator! *****\n")
+    peso = float(input("Ingresa tu peso en kilogramos: "))
+    estatura = float(input("Ingresa tu estatura en metros: "))
+    return peso, estatura
 
-imc = peso / estatura ** 2
 
-print ("\n---------------------------------------")
-print ("Tu indice de masa corporal es:", round(imc, 2))
-if (imc < 16):
-    print ("\n=> Delgadez severa.")
-elif (imc >= 16 and imc < 17):
-    print ("\n=>Delgadez moderada.")
-elif (imc >= 17 and imc < 18.5):
-    print ("\n=> Delgadez no pronunciada.")
-elif (imc >= 18.5 and imc < 25):
-    print ("\n=> Normal.")
-elif (imc >= 25 and imc < 30):
-    print ("\n=> Sobrepeso.")
-elif (imc >= 30 and imc < 35):
-    print ("\n=> obesidad tipo I")
-elif (imc >= 35 and imc < 40):
-    print ("\n=> obesidad tipo II")
-elif (imc >= 40):
-    print ("\n=> obesidad tipo III")
-print ("---------------------------------------")
+def calcular_imc(peso, estatura):
+    """
+    Calcula el IMC
+    """
+    imc = peso / estatura ** 2
 
-input()
+    print ("\n---------------------------------------")
+    print ("Tu indice de masa corporal es:", round(imc, 2))
+    if (imc < 16):
+        print ("\n=> Delgadez severa.")
+    elif (imc >= 16 and imc < 17):
+        print ("\n=>Delgadez moderada.")
+    elif (imc >= 17 and imc < 18.5):
+        print ("\n=> Delgadez no pronunciada.")
+    elif (imc >= 18.5 and imc < 25):
+        print ("\n=> Normal.")
+    elif (imc >= 25 and imc < 30):
+        print ("\n=> Sobrepeso.")
+    elif (imc >= 30 and imc < 35):
+        print ("\n=> obesidad tipo I")
+    elif (imc >= 35 and imc < 40):
+        print ("\n=> obesidad tipo II")
+    elif (imc >= 40):
+        print ("\n=> obesidad tipo III")
+    print ("---------------------------------------")
+
+
+
+opcion = 's'
+
+while opcion == 's'.lower():
+    limpiarPantalla()
+    peso, estatura = entrada_datos()
+    calcular_imc(peso, estatura)
+    opcion = input('\n>>> Desea calcular el IMC de nuevo? [s/n]: ')
+
+input("\n>>> Presiona una tecla para salir")
 limpiarPantalla()
